8f0a80da97de2edf13cd9dcf753a4c6b6e0798e3
andreeabea <andreea.beatrice2@yahoo.com>
2020-04-22 00:15:35 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+# initialize the path to the *original* input directory of images
+ORIG_INPUT_DATASET = "brain_tumor_dataset"
+# initialize the base path to the *new* directory that will contain
+# the images after computing the training and testing split
+BASE_PATH = "brain_tumor_new"
+# derive the training, validation, and testing directories
+TRAIN_PATH = os.path.sep.join([BASE_PATH, "training"])
+VAL_PATH = os.path.sep.join([BASE_PATH, "validation"])
+TEST_PATH = os.path.sep.join([BASE_PATH, "testing"])
+# define the amount of data that will be used training
+TRAIN_SPLIT = 0.8
+# the amount of validation data will be a percentage of the
+# *training* data
+VAL_SPLIT = 0.1
