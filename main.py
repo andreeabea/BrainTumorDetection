339b851c1ee4ffff696e4184969e479471237974
andreeabea <andreea.beatrice2@yahoo.com>
2020-05-18 15:50:40 +0300

    augmentation added

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 from imutils import paths
+from sklearn.metrics import classification_report
 
 import config
 import cv2
 import numpy as np
 
-from init import testGen
+from init import testGen, totalTest, BS
 
 new_model = tf.keras.models.load_model('models/DenseNet80')
 
@@ -15,6 +16,16 @@ new_model.summary()
 # Evaluate the restored model
 loss, acc = new_model.evaluate(testGen, verbose=2)
 print('Restored model, accuracy: {:5.2f}%'.format(100 * acc))
+
+print("[INFO] evaluating network...")
+testGen.reset()
+predIdxs = new_model.predict_generator(testGen, steps=(totalTest // BS) + 1)
+
+# for each image in the testing set we need to find the index of the
+# label with corresponding largest predicted probability
+predIdxs = np.argmax(predIdxs, axis=1)
+# show a nicely formatted classification report
+print(classification_report(testGen.classes, predIdxs, target_names=testGen.class_indices.keys()))
 
 images = list(paths.list_images(config.TEST_PATH))
 
